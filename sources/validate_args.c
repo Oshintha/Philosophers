87f3bd14e152fd7fc6e7fe375817af7c11c48c55
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r1p8.hive.fi>
2025-06-20 07:36:29 +0300

    corrected validation missing points

--- sources/validate_args.c
+++ sources/validate_args.c
@@ -6,7 +6,7 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 10:14:32 by aoshinth          #+#    #+#             */
-/*   Updated: 2025/05/24 10:14:32 by aoshinth         ###   ########.fr       */
+/*   Updated: 2025/06/20 00:35:26 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 void	print_usage_and_exit(void)
 {
 	printf("Usage: ./philo arg1 arg2 arg3 arg4 [arg5]\n");
-	printf("Arg     Name                        Type        Range\n");
-	printf("arg1    number_of_philosophers      mandatory   > 0\n");
-	printf("arg2    time_to_die (ms)            mandatory   > 0\n");
-	printf("arg3    time_to_eat (ms)            mandatory   > 0\n");
-	printf("arg4    time_to_sleep (ms)          mandatory   > 0\n");
-	printf("arg5    times_each_philo_must_eat   optional    > 0\n");
+	printf("Arg#    Name                        			Type        Range\n");
+	printf("arg1    number_of_philosophers      			mandatory   > 0\n");
+	printf("arg2    time_to_die (ms)            			mandatory   > 0\n");
+	printf("arg3    time_to_eat (ms)            			mandatory   > 0\n");
+	printf("arg4    time_to_sleep (ms)          			mandatory   > 0\n");
+	printf("arg5    number_of_times_each_philosopher_must_eat	optional    > 0\n");
 	exit(1);
 }
 
@@ -39,18 +39,17 @@ int	parse_positive_int(const char *str, size_t *num)
 int	validate_args(int argc, char **argv, t_simulation *sim)
 {
 	if (argc < 5 || argc > 6)
-		return (print_usage_and_exit(), 1);
+		return(print_usage_and_exit(), 1);
 	if (parse_positive_int(argv[1], &sim->philo_count))
-		return (1);
+		return (print_usage_and_exit(), 1);
 	if (parse_positive_int(argv[2], &sim->time_to_die_ms))
-		return (1);
+		return (print_usage_and_exit(), 1);
 	if (parse_positive_int(argv[3], &sim->time_to_eat_ms))
-		return (1);
+		return (print_usage_and_exit(), 1);
 	if (parse_positive_int(argv[4], &sim->time_to_sleep_ms))
-		return (1);
+		return (print_usage_and_exit(), 1);
 	sim->required_meals = 0;
 	if (argc == 6 && parse_positive_int(argv[5], &sim->required_meals))
-		return (1);
+		return (print_usage_and_exit(), 1);
 	return (0);
 }
-
